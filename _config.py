c057373973b4badb3018511db6c91bebe80a8284
Zach <73958348+af-zach@users.noreply.github.com>
2020-11-05 18:26:18 -0500

    Added function to extract downloaded zip to wow/addons folder

--- _config.py
+++ _config.py
@@ -4,12 +4,9 @@ wow_addons = "E:/World of Warcraft/_retail_/Interface/AddOns/"
 ## Input your path to store downloaded versions of Elvui
 save_path = "C:/Users/zach/Downloads/"
 
-
-
-
-
-
-## DO NOT EDIT BELOW HERE
+############################
+## DO NOT EDIT BELOW HERE ##
+############################
 
 ## Retail Elvui download page URL
 elvui_url = "https://www.tukui.org/download.php?ui=elvui"
@@ -22,6 +19,7 @@ elvui_version_xpath = '//*[@id="version"]/b[1]/text()'
 
 from lxml import html
 import requests
+import zipfile
 def get_public_version():        
     get_url = requests.get(elvui_url)
     tree = html.fromstring(get_url.content)
@@ -39,7 +37,7 @@ def get_local_version():
     with open(wow_addons+'/ElvUI/ElvUI.toc', 'rt') as myfile:
         for myline in myfile:
             mylines.append(myline)
-
+    
     ## Only keep the line which contains the version number
     version_line = mylines[2]
     myfile.close()
@@ -68,3 +66,7 @@ def download_elvui(url, save_path):
     ## Open method to open a file on your system and write the contents
     with open(save_path, "wb") as code:
         code.write(r.content)
+
+def unzip_elvui(path_to_zipfile, path_to_unzip):
+    with zipfile.ZipFile(path_to_zipfile, 'r') as zip_ref:
+        zip_ref.extractall(path_to_unzip)
--- check.py
+++ check.py
@@ -6,10 +6,11 @@ x = _config.get_local_version()
 y = _config.get_public_version()
 
 ## Compare versions
-if x < y:
-    print('Your version of ElvUI is up to date. Goodbye :D')
-    #sys.exit()
-elif x >= y:
+if x >= y:
+    print(f'Your version {x} of ElvUI is already up to date. Goodbye :D')
+    sys.exit()
+elif x < y:
+    print("Your version of ElvUI needs updated.")
     ## Build path to file for download and path for saving
     url = _config.elvui_download_url + "elvui-" + str(y) + ".zip"
     save_path = _config.save_path + "elvui-" + str(y) + ".zip"
@@ -18,6 +19,6 @@ elif x >= y:
     print(f"Downloading ElvUI version {y}")
     _config.download_elvui(url, save_path)
 
-    ## Call function to remove current ElvUI folders
-
     ## Call function to unzip new ElvUI version to wow/addons
+    print(f"Unzipping elvui-{y}.zip to wow/addons")
+    _config.unzip_elvui(save_path, _config.wow_addons)
