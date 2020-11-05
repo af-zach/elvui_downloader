a32dd4c3d35cb27a5a2fa7b113b06e0ee75ce1e1
Zach <73958348+af-zach@users.noreply.github.com>
2020-11-05 13:55:03 -0500

    Added download new ElvUI functions. Clean up files and code.

--- _config.py
+++ _config.py
@@ -1,8 +1,70 @@
-## Path to WOW addon folder
-wow_addons = "E:/World of Warcraft/_retail_/Interface/AddOns"
+## Input your path to local WOW addon folder
+wow_addons = "E:/World of Warcraft/_retail_/Interface/AddOns/"
+
+## Input your path to store downloaded versions of Elvui
+save_path = "C:/Users/zach/Downloads/"
+
+
+
+
+
+
+## DO NOT EDIT BELOW HERE
 
 ## Retail Elvui download page URL
 elvui_url = "https://www.tukui.org/download.php?ui=elvui"
 
+## Retail Elvui downloads URL
+elvui_download_url = "https://www.tukui.org/downloads/"
+
 ## Elvui version XPath
 elvui_version_xpath = '//*[@id="version"]/b[1]/text()'
+
+from lxml import html
+import requests
+def get_public_version():        
+    get_url = requests.get(elvui_url)
+    tree = html.fromstring(get_url.content)
+    version_list = tree.xpath(elvui_version_xpath)
+
+    ## Convert version_list to a float
+    get_version = convert_list_to_float(version_list)
+
+    ## Return version number as a list
+    return get_version
+
+def get_local_version():
+    mylines = []
+    ## Open ElvUI.toc store each line to a list 
+    with open(wow_addons+'/ElvUI/ElvUI.toc', 'rt') as myfile:
+        for myline in myfile:
+            mylines.append(myline)
+
+    ## Only keep the line which contains the version number
+    version_line = mylines[2]
+    myfile.close()
+    
+    ## Remove Version text characters and keep only version number
+    string_version = version_line.replace('## Version: ', '')
+
+    ## Convert string_version to a float
+    get_version = float(string_version)
+
+    return get_version
+
+def convert_list_to_float(version_list):
+    ## Convert a list to a float
+    ## Will not work properly if more than one item in list
+    for v in version_list:
+        float_version = float(v)
+    
+    ## Return version number as a float
+    return float_version
+
+def download_elvui(url, save_path):
+    ## Download the file contents in binary format
+    r = requests.get(url)
+
+    ## Open method to open a file on your system and write the contents
+    with open(save_path, "wb") as code:
+        code.write(r.content)
---
+++ check.py
@@ -0,0 +1,22 @@
+import sys
+import _config
+
+## Get public and local versions
+x = _config.get_local_version()
+y = _config.get_public_version()
+
+## Compare versions
+if x < y:
+    print('Your version of ElvUI is up to date. Goodbye :D')
+    #sys.exit()
+elif x >= y:
+    ## Build path to file for download and path for saving
+    url = _config.elvui_download_url + "elvui-" + str(y) + ".zip"
+    save_path = _config.save_path + "elvui-" + str(y) + ".zip"
+
+    ## Call function to download latest ElvUI
+    _config.download_elvui(url, save_path)
+
+    ## Call function to remove current ElvUI folders
+
+    ## Call function to unzip new ElvUI version to wow/addons
